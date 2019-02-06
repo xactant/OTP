9d0494f816905efe47efc473732f6520eb692ff4
xactant <xactant@gmail.com>
2019-02-06 14:02:50 -0500

    Added and updates code file header blocks

--- contract/contracts/Otp.sol
+++ contract/contracts/Otp.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.22 <0.6.0;
 /// ==========================================================================
 /// MODULE: OTP Ethereum Contract
-/// DESCRIPTION: Defines a Solidity contract that provides a means to host  
+/// DESCRIPTION: Defines a Solidity contract that provides a means to host
 /// versioned instances of the One Time Pad web assembly application
 ///
 /// Copyright 2019 by Xactant 42
@@ -11,6 +11,13 @@ pragma solidity >=0.4.22 <0.6.0;
 /// the rights to use, copy, modify, merge, publish, distribute, sublicense,
 /// and/or sell copies of the Software, and to permit persons to whom the
 /// Software is furnished to do so, subject to the following conditions:
+///  1. The origin of this source code must not be misrepresented; you must not
+///     claim that you wrote the original source code. If you use this source code
+///     in a product, an acknowledgment in the product documentation would be
+///     appreciated but is not required.
+///  2. Altered source versions must be plainly marked as such, and must not be
+///     misrepresented as being the original source code.
+///  3. This notice may not be removed or altered from any source distribution.
 ///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
--- wasm/otp.cc
+++ wasm/otp.cc
@@ -16,6 +16,13 @@
 /// the rights to use, copy, modify, merge, publish, distribute, sublicense,
 /// and/or sell copies of the Software, and to permit persons to whom the
 /// Software is furnished to do so, subject to the following conditions:
+///  1. The origin of this source code must not be misrepresented; you must not
+///     claim that you wrote the original source code. If you use this source code
+///     in a product, an acknowledgment in the product documentation would be
+///     appreciated but is not required.
+///  2. Altered source versions must be plainly marked as such, and must not be
+///     misrepresented as being the original source code.
+///  3. This notice may not be removed or altered from any source distribution.
 ///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
